4eca0b3362255a6659bae80ed97115d13ffc0feb
nguyenphuctue <tuenguyen29102001@gmail.com>
2023-07-24 23:29:02 +0700

    add edit function

--- src/main/java/com/example/todospringapp/controller/TaskController.java
+++ src/main/java/com/example/todospringapp/controller/TaskController.java
@@ -21,19 +21,41 @@ public class TaskController {
     public String index(Model model) {
         List<Task> tasks = taskRepository.getAllTasks();
         model.addAttribute("tasks", tasks);
-        int id = tasks.size() == 0 ? 1 : tasks.get(tasks.size() - 1).getId() + 1;
-        Task task = new Task(id);
-        model.addAttribute("task", task);
         return "index";
     }
 
-    @PostMapping("/add")
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        List<Task> tasks = taskRepository.getAllTasks();
+        int id = tasks.size() == 0 ? 1 : tasks.get(tasks.size() - 1).getId() + 1;
+        Task task = new Task(id);
+        model.addAttribute("task", task);
+        return "add-task";
+    }
+
+    @PostMapping("/add-task")
     public String addTask(@ModelAttribute("task") Task task, Model model) {
         taskRepository.addTask(task);
         return "redirect:/";
     }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") int id, Model model) {
+        Task task = taskRepository.findById(id);
+        System.out.println(task.toString());
+        model.addAttribute("task", task);
+        return "edit-task";
+    }
+
+    @PostMapping("/edit-task")
+    public String editTask(@ModelAttribute("task") Task task, Model model) {
+        System.out.println(task.toString());
+        taskRepository.editTask(task);
+        return "redirect:/";
+    }
+
     @GetMapping("/delete/{id}")
-    public String deleteTask(@PathVariable int id){
+    public String deleteTask(@PathVariable int id) {
         taskRepository.deleteTask(id);
         return "redirect:/";
     }
--- src/main/java/com/example/todospringapp/repository/TaskRepository.java
+++ src/main/java/com/example/todospringapp/repository/TaskRepository.java
@@ -5,35 +5,48 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Repository
 public class TaskRepository {
+
     List<Task> taskList = new ArrayList<>();
 
+    public TaskRepository() {
+        taskList.add(new Task(1, "Title 1", "This is content 1"));
+        taskList.add(new Task(2, "Title 2", "This is content 2"));
+    }
 
-    public Task findById(int id){
+    public Task findById(int id) {
         Task task = null;
-        for (Task t : taskList){
-            if(t.getId() == id){
+        for (Task t : taskList) {
+            if (t.getId() == id) {
                 task = t;
             }
         }
         return task;
     }
 
-    public List<Task> getAllTasks(){
+    public List<Task> getAllTasks() {
         return taskList;
     }
 
-    public void addTask(Task task){
+    public void addTask(Task task) {
         taskList.add(task);
     }
 
-    public void editTask(Task task){
-
+    public void editTask(Task task) {
+        Task t = findById(task.getId());
+        if (t != null) {
+            taskList.remove(t);
+            taskList.add(task);
+            System.out.println("Edit successful");
+        }else{
+            System.out.println("Edit unsuccessful");
+        }
     }
 
-    public void deleteTask(int id){
+    public void deleteTask(int id) {
         Task task = findById(id);
-        if(task != null) taskList.remove(task);
+        if (task != null) taskList.remove(task);
     }
 }
